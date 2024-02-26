8a2e76a322cbb2e9b533548701c0ec8a1d2e09d1
Janki1410 <jankigabani10@gmail.com>
2024-02-26 15:54:24 +0530

    added common repo in mvc

--- MVC/Repostories/CommonRepository.cs
+++ MVC/Repostories/CommonRepository.cs
@@ -2,11 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Npgsql;  
 
 namespace MVC.Repostories
 {
     public class CommonRepository
     {
-        
+        protected NpgsqlConnection conn; 
+
+       public CommonRepository()
+        {
+            IConfiguration myConfig = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+
+            conn = new NpgsqlConnection(myConfig.GetConnectionString("DefaultConnection"));
+        }
     }
 }

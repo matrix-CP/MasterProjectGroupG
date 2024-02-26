a5008c0d0d7bc2709f395e5f25363647d815b26c
Shivang-Dhimmar <shivang3921dhimmar@gmail.com>
2024-02-26 14:45:34 +0530

    Initial Commit

---
+++ MVC/Controllers/MVCKendoCompController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MVC.Controllers
+{
+    // [Route("[controller]")]
+    public class MVCKendoCompController : Controller
+    {
+        private readonly ILogger<MVCKendoCompController> _logger;
+
+        public MVCKendoCompController(ILogger<MVCKendoCompController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
---
+++ MVC/Controllers/SimpleMVCController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MVC.Controllers
+{
+    // [Route("[controller]")]
+    public class SimpleMVCController : Controller
+    {
+        private readonly ILogger<SimpleMVCController> _logger;
+
+        public SimpleMVCController(ILogger<SimpleMVCController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
